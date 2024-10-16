b31f3fc826732b0a034b1cdb57b5feeb0cb4b76d
Kaua Mateus Wall <151979897+KauaMateusWall@users.noreply.github.com>
2024-10-16 20:44:37 -0300

    Arrumado

--- src/main/java/org/example/pi_primo/TelaCadastroProduto.java
+++ src/main/java/org/example/pi_primo/TelaCadastroProduto.java
@@ -1,9 +1,22 @@
 package org.example.pi_primo;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.time.LocalDate;
+
+import static org.example.pi_primo.HelloController.*;
 
 public class TelaCadastroProduto {
     public Button cadastrarButton;
@@ -13,31 +26,52 @@ public class TelaCadastroProduto {
     public TextField nomeText;
     public TextArea descricaoText;
 
-    public void voltarClicked(ActionEvent actionEvent) {
-        // todo:Fazer toda a lógica de volar para a ultima página
+    public void voltarClicked(ActionEvent event) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("paginaLogin.fxml"));
+            Parent root = fxmlLoader.load();
+
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            Scene scene = new Scene(root, 640, 400);
+
+            stage.setTitle("Empréstimo VK - Cadastro");
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    public void cadastrarClicked(ActionEvent actionEvent) {
-        String getDescricao=descricaoText.getText();
-        String getTipo=tipoText.getText();
-        String getPreco=precoText.getText();
-        String getNome=nomeText.getText();
+    public void cadastrarClicked(ActionEvent actionEvent) throws SQLException {
+        HelloController helloController = new HelloController();
+        try {
+            helloController.conection();
+            String InserSQL = "INSERT INTO Produto (nome, descricao, categoria_Produto, preco) VALUES (?, ?, ?, ?)";
+            try (PreparedStatement InsertSMT = conn.prepareStatement(InserSQL)) {
+                String nome = nomeText.getText();
+                String preco = precoText.getText();
+                String descricao = descricaoText.getText();
 
-        if(getDescricao.isEmpty()||getTipo.isEmpty()||getNome.isEmpty()||getPreco.isEmpty()){
-            HelloController.showAlert("Cadastro incompleto",
-                    "Porfavor preecha todos os campos antes de registrar o produto");
-            return;
+
+                if (nome.isEmpty() || preco.isEmpty() || descricao == null) {
+                    showAlert("Campos vazios", "Por favor, preencha todos os campos.");
+                    return;
+                }
+
+                InsertSMT.setString(1, nome);
+                InsertSMT.setString(2, preco);
+                InsertSMT.setString(3, descricao);
+
+                InsertSMT.execute();
+                showAlert("Cadastro bem-sucedido", "Cadastro de " + nome + " foi um sucesso!!!");
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            helloController.closeConection();
         }
 
-        try{
-            double preco = Double.parseDouble(getPreco);
 
-        } catch (NumberFormatException _){
-            HelloController.showAlert("Valor inválido","Passe um valor válido para o campo \"Preço\"");
-            return;
-        }
-
-        // todo:Fazer toda a lógica de cadastro de produto
     }
-
 }
