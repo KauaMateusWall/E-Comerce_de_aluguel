a0fc70b066291ea1e63d051eb709e1f4de5dad6c
monteji727 <monteji.barj@gmail.com>
2024-10-21 21:34:35 -0300

    tela do produto

---
+++ src/main/java/org/example/pi_primo/TelaProduto.java
@@ -0,0 +1,20 @@
+package org.example.pi_primo;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
+public class TelaProduto {
+    public Button alugarButton;
+    public Label precoText;
+    public Label nomeProdutoText;
+    public ImageView ProdutoImage;
+    public Button voltarButton;
+
+    public void alugarClicked(ActionEvent actionEvent) {
+    }
+
+    public void voltarClicked(ActionEvent actionEvent) {
+    }
+}
