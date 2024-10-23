cdc5a0a64e15a5e9ded1c3c8b90e13747f2a21d1
monteji727 <monteji.barj@gmail.com>
2024-10-23 19:19:07 -0300

    tela menu inicial feitra

--- src/main/java/org/example/pi_primo/TelaProduto.java
+++ src/main/java/org/example/pi_primo/TelaProduto.java
@@ -1,20 +1,26 @@
 package org.example.pi_primo;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 
 public class TelaProduto {
-    public Button alugarButton;
-    public Label precoText;
-    public Label nomeProdutoText;
-    public ImageView ProdutoImage;
-    public Button voltarButton;
+    @FXML public Button alugarButton;
+    @FXML public Button voltarButton;
 
+    @FXML public Label precoText;
+    @FXML public Label nomeProdutoText;
+
+    @FXML public ImageView ProdutoImage;
+    public Label descricaoText;
+
+    @FXML
     public void alugarClicked(ActionEvent actionEvent) {
     }
 
+    @FXML
     public void voltarClicked(ActionEvent actionEvent) {
     }
 }
