496e624d4becb44812bc90036901f65981b5daec
Kaua Mateus Wall <151979897+KauaMateusWall@users.noreply.github.com>
2024-10-28 21:23:02 -0300

    Arrumado

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,16 +2,25 @@ module org.example.pi_primo {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
-    requires spring.context.support;
-    requires commons.email;
-    requires javafx.graphics;
-    requires java.desktop;
+    requires spring.boot;
+    requires spring.boot.autoconfigure;
+    requires spring.context;
+    requires spring.beans;
+    requires jakarta.mail;
+    requires static lombok;
 
+    requires java.desktop;
+    requires spring.core;
+    requires spring.context.support;
+    requires java.mail;
+    requires commons.email; // Inclui java.awt e outros
 
     opens org.example.pi_primo to javafx.fxml;
     exports org.example.pi_primo;
     exports org.example.pi_primo.model;
     opens org.example.pi_primo.model to javafx.fxml;
-    exports org.example.pi_primo.sevice;
-    opens org.example.pi_primo.sevice to javafx.fxml;
+    exports org.example.pi_primo.service; // Corrigido de "sevice" para "service"
+    opens org.example.pi_primo.service to javafx.fxml;
+    exports org.example.pi_primo.emailServico;
+    opens org.example.pi_primo.emailServico to javafx.fxml;
 }
