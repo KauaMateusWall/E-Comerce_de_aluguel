5920a4815a34987f189ac2cd1ff8b008978eee29
Kaua Mateus Wall <151979897+KauaMateusWall@users.noreply.github.com>
2024-11-06 20:33:01 -0300

    Arrumado

--- src/main/java/org/example/pi_primo/model/Session.java
+++ src/main/java/org/example/pi_primo/model/Session.java
@@ -3,5 +3,5 @@ package org.example.pi_primo.model;
 public class Session {
     public static Cliente usuario= new Cliente(0,"NULL","NULL","NULL","NULL","NULL","NULL");
     public static Cliente dono= new Cliente(0,"NULL","NULL","NULL","NULL","NULL","NULL");
-    public static Produto produto= new Produto("NULL","NULL","NULL",0,0,0);
+    public static Produto produto= new Produto("NULL","NULL","NULL",0,0,0,"NULL");
 }
