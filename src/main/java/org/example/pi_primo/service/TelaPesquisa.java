72616e9efa06eaa0fc365e42b4b839ad61bc1470
monteji727 <monteji.barj@gmail.com>
2024-11-06 20:30:34 -0300

    melhoria de páginas e método de fechar e abrir
    fiz algumas alterações no código para poder ter uma melhor visualização para o usuário e também fiz o programa iniciar sempre na tela do menu para ter que fazer login ou cadastrar.

---
+++ src/main/java/org/example/pi_primo/service/TelaPesquisa.java
@@ -0,0 +1,60 @@
+package org.example.pi_primo.service;
+
+import javafx.event.ActionEvent;
+import javafx.scene.Scene;
+import javafx.scene.control.Pagination;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import org.example.pi_primo.HelloApplication;
+import org.example.pi_primo.config.ConexaoDB;
+import org.example.pi_primo.model.Produto;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class TelaPesquisa {
+    HelloApplication helloApplication=new HelloApplication();
+    public Scene mainScene;
+    public TextField pesquisaText;
+    public TableView produtosTableView;
+    public Pagination pagination;
+
+    public void voltarClicked(ActionEvent actionEvent) {
+        helloApplication.loadScreen("paginaMenu.fxml","VK", mainScene);
+    }
+
+    public void pesquisarKeyPressed(KeyEvent keyEvent) {
+        if(keyEvent.getCode()== KeyCode.ENTER)
+            pesquisarClicked();
+    }
+
+    public void pesquisarClicked() {
+        produtosTableView.getItems().clear();
+
+        String SELECT="SELECT * FROM produtos WHERE nome LIKE %?% and situação=\"DISPONÍVEL\";";
+        try(PreparedStatement stmt=ConexaoDB.conn.prepareStatement(SELECT)){
+
+            stmt.setString(1,pesquisaText.getText());
+
+            ResultSet rs=stmt.executeQuery();
+            while(rs.next()){
+                int id= rs.getInt("id");
+                String nome=rs.getString("nome");
+                String tipo= rs.getString("tipo");
+                double preco=rs.getDouble("preco");
+                int qtn_emprestimos= rs.getInt("quantidadeDeEmprestimos");
+                String situ=rs.getString("situação");
+                String desc=rs.getString("descricao");
+
+                Produto produto= new Produto(nome,tipo,desc,qtn_emprestimos,preco, situ,id);
+
+                produtosTableView.getItems().add(produto);
+            }
+        } catch (SQLException e){
+
+        }
+    }
+}
