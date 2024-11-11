1162b3440108d77fe7b85f5ba82697fe26c6d11b
Kaua Mateus Wall <151979897+KauaMateusWall@users.noreply.github.com>
2024-11-11 20:03:27 -0300

    Arrumado

--- src/main/java/org/example/pi_primo/service/TelaProduto.java
+++ src/main/java/org/example/pi_primo/service/TelaProduto.java
@@ -13,41 +13,42 @@ import javafx.stage.Stage;
 import org.example.pi_primo.HelloApplication;
 import org.example.pi_primo.config.ConexaoDB;
 import org.example.pi_primo.model.Session;
-
 import java.io.IOException;
 
 public class TelaProduto {
-    public Scene mainScene;
+
     HelloApplication helloApplication = new HelloApplication();
     ConexaoDB helloController = new ConexaoDB();
 
-    @FXML public Button alugarButton;
-    @FXML public Button voltarButton;
 
-    @FXML public Label precoText;
-    @FXML public Label nomeProdutoText;
 
-    @FXML public ImageView ProdutoImage;
-    public Label descricaoText;
-    public Label donoText;
+    @FXML
+    public Scene mainScene;
+    @FXML
+    public Label NomeTXT;
+    @FXML
+    public Label PrecoTXT;
+    @FXML
+    public Label ProTXT;
+    @FXML
+    public Label DescricaoTXT;
+
 
     @FXML
     public void initialize() {
-        precoText.setText(String.valueOf(Session.produto.getPreco()));
-        nomeProdutoText.setText(Session.produto.getNome());
-        donoText.setText(Session.dono.getNome());
-        descricaoText.setText(Session.produto.getDescricao());
+        PrecoTXT.setText(String.valueOf(Session.produto.getPreco()));
+        NomeTXT.setText(Session.produto.getNome());
+        ProTXT.setText(Session.dono.getNome());
+        DescricaoTXT.setText(Session.produto.getDescricao());
         
     }
 
     @FXML
     public void alugarClicked(ActionEvent actionEvent) {
-
-
     }
 
     @FXML
-    public void voltarClicked(ActionEvent actionEvent) {
-        helloApplication.loadScreen("paginaMenu.fxml","Empréstimo VK - Menu", mainScene);
+    public void voltarClicked(ActionEvent event) {
+        helloApplication.loadScreen("paginaMenu.fxml","Empréstimo VK - Menu",mainScene);
     }
 }
