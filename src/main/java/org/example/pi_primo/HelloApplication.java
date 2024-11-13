19b436dfb92bc65d63d2fa8b69f4719c5b16cf89
monteji727 <monteji.barj@gmail.com>
2024-11-13 20:48:47 -0300

    modelo do pedido

--- src/main/java/org/example/pi_primo/HelloApplication.java
+++ src/main/java/org/example/pi_primo/HelloApplication.java
@@ -15,7 +15,7 @@ public class HelloApplication extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("paginalogin.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("paginaMeusPedidos.fxml"));
         Scene scene = fxmlLoader.load();
         stage.setTitle("Empréstimo VK - Login");
         stage.setScene(scene);
--- src/main/java/org/example/pi_primo/service/TelaMeusPedidos.java
+++ src/main/java/org/example/pi_primo/service/TelaMeusPedidos.java
@@ -5,12 +5,6 @@ import javafx.scene.control.TableView;
 import org.example.pi_primo.HelloApplication;
 import org.example.pi_primo.config.ConexaoDB;
 import org.example.pi_primo.model.Pedido;
-import org.example.pi_primo.model.Produto;
-
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 
 public class TelaMeusPedidos {
     public TableView<Pedido> pedidoTable;
@@ -23,7 +17,7 @@ public class TelaMeusPedidos {
     }
 
     public void voltarClicked() {
-        helloApplication.loadScreen("paginaUsuario.fxml","VK",mainScene);
+        helloApplication.loadScreen("paginaMeuUsuario.fxml","VK",mainScene);
     }
 
     public void pedidoTabelaClicked() {
@@ -34,6 +28,5 @@ public class TelaMeusPedidos {
                         "INNER JOIN cliente c ON c.id=ped.cliente_id " +
                         "INNER JOIN cliente prop ON prop.id=ped.proprietario_id " +
                         "INNER JOIN produto p ON p.id=ped.produto_id ORDER BY data ASC;";
-
     }
 }
