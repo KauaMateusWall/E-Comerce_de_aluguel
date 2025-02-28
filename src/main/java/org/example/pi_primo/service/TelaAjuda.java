b3f41fef008d0fb3517e74312f8d0459f2625a56
Kaua Mateus Wall <151979897+KauaMateusWall@users.noreply.github.com>
2025-02-27 21:08:19 -0300

    Arrumado

--- src/main/java/org/example/pi_primo/service/TelaAjuda.java
+++ src/main/java/org/example/pi_primo/service/TelaAjuda.java
@@ -7,13 +7,6 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 
-import com.twilio.Twilio;
-import com.twilio.converter.Promoter;
-import com.twilio.rest.api.v2010.account.Message;
-import com.twilio.type.PhoneNumber;
-
-import java.net.URI;
-import java.math.BigDecimal;
 
 public class TelaAjuda {
 
