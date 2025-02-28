06a3af5a3d44379cb88d98db848caa6c69709c39
Kaua Mateus Wall <151979897+KauaMateusWall@users.noreply.github.com>
2025-02-27 21:03:53 -0300

    Arrumado

--- src/main/java/org/example/pi_primo/service/TelaAjuda.java
+++ src/main/java/org/example/pi_primo/service/TelaAjuda.java
@@ -7,6 +7,14 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 
+import com.twilio.Twilio;
+import com.twilio.converter.Promoter;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
+
+import java.net.URI;
+import java.math.BigDecimal;
+
 public class TelaAjuda {
 
     private static final String ACCOUNT_SID = "AC366ac1ca7052def5eb6836fb7ab41d52";
@@ -43,4 +51,7 @@ public class TelaAjuda {
             System.err.println("Erro ao enviar mensagem: " + e.getMessage());
         }
     }
+
+
+
 }
